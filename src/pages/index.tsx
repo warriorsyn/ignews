e3bdbf181c0d967daee2b92599f881dcbc9e67f1
João Victor de Andrade <joao.victor@primesquad.com.br>
2021-05-16 22:30:07 -0300

    Home page layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,27 @@
 import Head from 'next/head';
-
- 
+import styles from './home.module.scss';
 
 function Home() {
   return (
     <>
-    <Head>
-      <title>Home | ig.news</title>
-    </Head>
-      <h1>Hello world</h1>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.homeContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>  
+          <h1>New about the <span>React</span> world.</h1>
+          <p>
+            Get access to all publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>  
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main> 
     </>
   )
 }
-
 
 export default Home;
