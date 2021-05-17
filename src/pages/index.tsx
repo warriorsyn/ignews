9d1cc1b9d05b3d03666fc7dd80b8952c3e43fcf3
João Victor de Andrade <joao.victor@primesquad.com.br>
2021-05-16 23:58:57 -0300

    Subscribe btn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
 
 function Home() {
@@ -16,6 +17,8 @@ function Home() {
             Get access to all publications <br />
             <span>for $9.90 month</span>
           </p>
+
+          <SubscribeButton />
         </section>  
 
         <img src="/images/avatar.svg" alt="Girl coding" />
