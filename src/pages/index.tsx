9b0bc48a43883a2e7731e7a25474f96a9b529e50
João Victor de Andrade <joao.victor@primesquad.com.br>
2021-05-19 00:00:36 -0300

    Get price from  ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
+import { GetServerSideProps, GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
+import { stripe } from '../services/Stripe';
 
-function Home() {
+interface HomeProps {
+  product: { priceId: string, amount: number }
+}
+function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -15,7 +20,7 @@ function Home() {
           <h1>New about the <span>React</span> world.</h1>
           <p>
             Get access to all publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
           <SubscribeButton />
@@ -25,6 +30,24 @@ function Home() {
       </main> 
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IseljGbFNzxPe91yLjdL5zN', {
+    expand: ['product']
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: price.unit_amount / 100
+  };
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 8 * 8
+  }
 }
 
 export default Home;
