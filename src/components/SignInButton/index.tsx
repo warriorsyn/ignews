01436c3d4c16f61825b84fddeebec846ab46b536
João Victor de Andrade <joao.victor@primesquad.com.br>
2021-05-22 13:17:38 -0300

    Authentication with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,15 +1,20 @@
+import { signIn, useSession } from 'next-auth/client';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+
+    const [session] = useSession();
+
+    const isUserLoggedIn = session;
 
     return (
-        <button type="button" className={styles.signInButton}>
+        <button type="button" className={styles.signInButton} onClick={() => !isUserLoggedIn && signIn('github')}>
             <FaGithub color={isUserLoggedIn ? '#4eba41' : '#eba417'} />
-            { !isUserLoggedIn ? 'Sign in with Github' : 'Diego Fernandes' }
+            { !isUserLoggedIn ? 'Sign in with Github' : isUserLoggedIn.user.name}
             { isUserLoggedIn && <FiX  color="#737380" className={styles.closeIcon}/> }
         </button>
     )
