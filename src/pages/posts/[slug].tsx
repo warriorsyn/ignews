6381e933f01337c844c8250e4205f640ad0e1e23
João Victor de Andrade <joao.victor@primesquad.com.br>
2021-06-05 17:24:51 -0300

    Fix imports

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -2,8 +2,8 @@ import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/client";
 import Head from "next/head";
 import { RichText } from "prismic-dom";
-import { getPrismicClient } from "../../services/prismic";
 
+import { getPrismicClient } from "../../services/prismic";
 import styled from './post.module.scss';
 
 
